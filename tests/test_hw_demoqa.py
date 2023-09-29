f9e3c035f80b457f9b2a752222ecbb6dc9b4e699
max <Maxunris@gmail.com>
2023-09-29 22:56:52 +0300

    main #3 залита основа дз

--- tests/test_hw_demoqa.py
+++ tests/test_hw_demoqa.py
@@ -1,3 +1,5 @@
+import time
+
 from selene import browser, be, have
 import os
 
@@ -12,5 +14,17 @@ def test_registration_form():
     browser.element("#userNumber").type("79991231234")
     browser.element("#dateOfBirthInput").click()
     browser.element('.react-datepicker__month-select').click().element('option[value="1"]').click()
-    browser.element()
+    browser.element('.react-datepicker__year-select').click().element('option[value="1998"]').click()
+    browser.element('.react-datepicker__day--026').click()
+    browser.element('#subjectsInput').type('Engl').press_enter()
+    browser.element('label[for="hobbies-checkbox-1"]').click()
+    browser.element('label[for="hobbies-checkbox-2"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('picture/fier.webp'))
+    browser.element('#currentAddress').type('Pyshkina-kolotushkina, Moscow, Russia')
+    browser.element('#react-select-3-input').type('Utt').press_enter()
+    browser.element('#react-select-4-input').type('Lu').press_enter()
+    browser.element('#submit').click()
+    time.sleep(20.0)
+
+
 
